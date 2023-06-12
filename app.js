d8fb05db93e54870eda6e7b6824a20b528847a90
denkochev <denkochev78@gmail.com>
2023-06-12 17:12:14 +0200

    Add register method

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const {MongoClient} = require('mongodb');
+require("dotenv").config();
 
+// Порт сервера
+const port = process.env.PORT;
 const app = express();
 app.use(express.json());
+
+const uri = process.env.MONGO_URL;
+const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+
+// Middleware to establish MongoDB connection
+app.use(async (req, res, next) => {
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB');
+        req.dbClient = client;
+        next();
+    } catch (error) {
+        console.error('Error connecting to MongoDB', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
 
 // Метод для генерації хешу пароля
 async function hashPassword(password) {
@@ -20,39 +41,46 @@ async function verifyPassword(password, hashedPassword) {
 
 // Метод для генерації JWT токена
 function generateToken(userId) {
-    const secretKey = 'your_secret_key'; // Замініть на свій секретний ключ
+    const secretKey = process.env.SECRET_KEY; // Замініть на свій секретний ключ
     const expiresIn = '1h';
 
-    const token = jwt.sign({ userId }, secretKey, { expiresIn });
+    const token = jwt.sign({userId}, secretKey, {expiresIn});
     return token;
 }
 
 // Маршрут для реєстрації нового користувача
 app.post('/register', async (req, res) => {
-    const { username, password } = req.body;
+    const {username, password} = req.body;
 
     // Генерація хешу пароля
     const hashedPassword = await hashPassword(password);
 
     // Збереження користувача в базі даних (в цьому прикладі просто виводимо хеш пароля)
-    console.log(`Username: ${username}`);
-    console.log(`Hashed Password: ${hashedPassword}`);
-
-    res.status(201).send('User registered successfully.');
+    try {
+        const db = req.dbClient.db('Authorizations');
+        const collection = db.collection('users');
+        const result = await collection.insertOne({username, hashedPassword});
+        res.status(201).send(`User registered successfully. [ insertedId = ${result.insertedId} ]`);
+    } catch (error){
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 // Маршрут для авторизації користувача
 app.post('/login', async (req, res) => {
-    const { username, password } = req.body;
+    const {username, password} = req.body;
 
-    // Перевірка користувача в базі даних (в цьому прикладі просто виконуємо перевірку пароля)
+    // Перевірка користувача в базі даних
+    const db = req.dbClient.db('Authorizations');
+
     const isValidPassword = await verifyPassword(password, hashedPasswordFromDatabase); // Потрібно отримати хеш пароля з бази даних
 
     if (isValidPassword) {
         // Генерація JWT токена
         const token = generateToken(userId); // Потрібно отримати ID користувача з бази даних
 
-        res.json({ token });
+        res.json({token});
     } else {
         res.status(401).send('Invalid username or password.');
     }
@@ -77,10 +105,18 @@ app.get('/protected', (req, res) => {
     }
 });
 
-// Порт сервера
-const port = 3000;
+// Close the MongoDB connection when the application is shutting down
+process.on('SIGINT', async () => {
+    try {
+        await client.close();
+        console.log('MongoDB connection closed');
+        process.exit(0);
+    } catch (error) {
+        console.error('Error closing MongoDB connection', error);
+        process.exit(1);
+    }
+});
 
-// Запуск сервера
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
